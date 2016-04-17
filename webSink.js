61b0063bac49f4c1ef3f745062532fbde1ec8145
Damian Krychowski <damian.krychowski@gmail.com>
2016-04-17 14:31:32 +0200

    Walking skeleton - step 2 - implementation

--- webSink.js
+++ webSink.js
@@ -1,3 +1,8 @@
+var Promise = require("bluebird");
+var express = require("express");
+var logger = require("winston");
+var config = require("./config");
+
 module.exports.create = function (config) {
     var webSink = {
         startAsync: startAsync,
@@ -5,13 +10,35 @@ module.exports.create = function (config) {
         exposeData: exposeData
     };
 
+    var server = express();
+    var currentState;
+    var sensorHttpServer;
+
+    server.get("/sensors", function (req, res) {
+        res.send(currentState);
+    });
+
     function startAsync() {
+        return new Promise(function (resolve, reject) {
+            sensorHttpServer = server.listen(config.webSinkPort, function (err) {
+                if (err) { reject(err); }
+                else { resolve(); }
+            });
+        }).then(() => logger.info("Sensor web sink is started on port " + config.webSinkPort));
     }
 
     function stopAsync() {
+        return new Promise(function (resolve, reject) {
+            sensorHttpServer.close(function (err) {
+                if (err) { reject(err); }
+                else { resolve(); }
+            });
+
+        }).then(() => logger.info("Sensor web sink on " + config.webSinkPort + " port is stopped."));
     }
 
     function exposeData(newValue) {
+        currentState = newValue;
     }
 
     return webSink;
