7b40d9175c818af4d2edc32b21c5c49f0848cc3d
Damian Krychowski <damian.krychowski@gmail.com>
2016-04-23 13:02:49 +0200

    New features added to sensor

--- test/sensor.js
+++ test/sensor.js
@@ -1,42 +1,80 @@
 var Promise = require("bluebird");
 var net = require("net");
 var logger = require("winston");
+var frameFactory = require("../frame");
 
-module.exports.create = function(){
+module.exports.create = function () {
     var sensor = {
         withServerHost: withServerHost,
         withServerPort: withServerPort,
-        connectAsync: connectAsync
+        withFramesToRespondWith: withFramesToRespondWith,
+        withSensorId: withSensorId,
+        withSensorValue: withSensorValue,
+        connectAsync: connectAsync,
+        destroyConnection: destroyConnection,
+        dataFrame: dataFrame
     }
-    
+
     var serverHost, serverPort;
     var client = new net.Socket();
     var identity;
-    var receivedData = [];
     var receivedDataCounter = 0;
-    
-    function withServerHost(host){
+    var framesToRespondWith;
+    var framesToRespondWithIndex = 0;
+    var sensorId;
+    var sensorValue;
+
+    function withServerHost(host) {
         serverHost = host;
         return sensor;
     }
-    
-    function withServerPort(port){
+
+    function withServerPort(port) {
         serverPort = port;
         return sensor;
     }
+
+    function withFramesToRespondWith(frames) {
+        framesToRespondWith = frames.slice();
+        return sensor;
+    }
+
+    function withSensorId(id) {
+        sensorId = id;
+        return sensor;
+    }
+
+    function withSensorValue(value) {
+        sensorValue = value;
+        return sensor;
+    }
     
-    client.on("data", function(data) {
-        receivedData.push(data);
+    function dataFrame(){
+        return frameFactory
+            .createDataFrame(sensorId, sensorValue)
+            .getBytes();
+    }
+
+    client.on("data", function (data) {
         receivedDataCounter++;
-        
-        if(receivedDataCounter === 1){
+
+        if (receivedDataCounter === 1) {
             sensor.firstReceivedFrame = data;
+        }
+
+        if (receivedDataCounter === 2) {
+            sensor.secondReceivedFrame = data;
+        }
+
+        if (framesToRespondWith && framesToRespondWith[framesToRespondWithIndex]) {
+            client.write(framesToRespondWith[framesToRespondWithIndex]);
+            framesToRespondWithIndex++;
         }
     });
 
-     function connectAsync() {
-        return new Promise(function(resolve, reject) {
-            client.connect(serverPort, serverHost, function(err) {
+    function connectAsync() {
+        return new Promise(function (resolve, reject) {
+            client.connect(serverPort, serverHost, function (err) {
                 if (err) {
                     logger.error("Client error " + err.toString());
                     reject(err);
@@ -48,6 +86,10 @@ module.exports.create = function(){
             });
         });
     }
-    
+
+    function destroyConnection() {
+        client.destroy();
+    }
+
     return sensor;
 }
