3026b4a40ef72f9fc3ae0bb0ac21637dcacda681
Damian Krychowski <damian.krychowski@gmail.com>
2016-04-23 11:38:29 +0200

    End-to-end tests for different sensor communication scenarios - 1

--- test/endToEnd.js
+++ test/endToEnd.js
@@ -55,6 +55,135 @@ describe("Full end to end tests", function () {
             .then(() => sensor.firstReceivedFrame.should.be.eql(confirmationFrame()));
     });
 
+    it("sensorAgent should accept connections from more than one sensor", function () {
+        var firstSensor = sensorFactory.create()
+            .withServerHost("localhost")
+            .withServerPort(6002);
+
+        var secondSensor = sensorFactory.create()
+            .withServerHost("localhost")
+            .withServerPort(6002);
+
+        return sensorAgent
+            .startAsync()
+            .then(() => firstSensor.connectAsync())
+            .then(() => secondSensor.connectAsync())
+            .delay(50)
+            .then(() => firstSensor.firstReceivedFrame.should.be.eql(confirmationFrame()))
+            .then(() => secondSensor.firstReceivedFrame.should.be.eql(confirmationFrame()));
+    });
+
+    it("sensorAgent should accept second connection from the sensor if the first connection was lost", function () {
+        var sensor = sensorFactory.create()
+            .withServerHost("localhost")
+            .withServerPort(6002);
+
+        return sensorAgent
+            .startAsync()
+            .then(() => sensor.connectAsync())
+            .delay(25)
+            .then(() => sensor.destroyConnection())
+            .delay(25)
+            .then(() => sensor.connectAsync())
+            .delay(50)
+            .then(() => sensor.firstReceivedFrame.should.be.eql(confirmationFrame()))
+            .then(() => sensor.secondReceivedFrame.should.be.eql(confirmationFrame()))
+    });
+
+    it("sensorAgent should send STATUS frame to acquire data from the sensor", function () {
+        var sensor = sensorFactory.create()
+            .withServerHost("localhost")
+            .withServerPort(6002);
+
+        return sensorAgent
+            .startAsync()
+            .then(() => sensor.connectAsync())
+            .delay(50)
+            .then(() => sensorAgent.collectDataFromSensorsAsync())
+            .delay(50)
+            .then(() => sensor.firstReceivedFrame.should.be.eql(confirmationFrame()))
+            .then(() => sensor.secondReceivedFrame.should.be.eql(statusFrame()))
+    });
+    
+    function statusFrame(){
+        return frameFactory
+            .createStatusFrame()
+            .getBytes();
+    }
+
+    it("sensorAgent should close connection with the sensor on receiving NACK frame", function () {
+        var sensor = sensorFactory.create()
+            .withServerHost("localhost")
+            .withServerPort(6002)
+            .withFramesToRespondWith([
+                nackFrame({ errorCode: 0x01 })
+            ]);
+
+        return sensorAgent
+            .startAsync()
+            .then(() => sensor.connectAsync())
+            .delay(50)
+            .then(() => sensorAgent.collectDataFromSensorsAsync())
+            .delay(50)
+            .then(() => sensor.isConnected().should.not.be.true());
+    });
+
+    function nackFrame(nackData) {
+        return frameFactory
+            .createNackFrame(nackData.errorCode)
+            .getBytes();
+    }
+
+    it("sensorAgent should close connection with the sensor on receiving unknown frame", function () {
+        var sensor = sensorFactory.create()
+            .withServerHost("localhost")
+            .withServerPort(6002)
+            .withFramesToRespondWith([
+                new Buffer([0xFF, 0xFF])
+            ]);
+
+        return sensorAgent
+            .startAsync()
+            .then(() => sensor.connectAsync())
+            .delay(50)
+            .then(() => sensorAgent.collectDataFromSensorsAsync())
+            .delay(50)
+            .then(() => sensor.isConnected().should.not.be.true());
+    });
+
+    it("sensorAgent should expose data acquired from the sensors", function () {
+        var firstSensor = sensorFactory.create()
+            .withServerHost("localhost")
+            .withServerPort(6002)
+            .withSensorId(1)
+            .withSensorValue(25)
+            .withFramesToRespondWith([
+                firstSensor.dataFrame()
+            ]);
+
+        var secondSensor = sensorFactory.create()
+            .withServerHost("localhost")
+            .withServerPort(6002)
+            .withSensorId(2)
+            .withSensorValue(10)
+            .withFramesToRespondWith([
+                secondSensor.dataFrame()
+            ]);
+
+        return sensorAgent
+            .startAsync()
+            .then(() => firstSensor.connectAsync())
+            .then(() => secondSensor.connectAsync())
+            .delay(50)
+            .then(() => sensorAgent.collectDataFromSensorsAsync())
+            .delay(50)
+            .then(() => webSinkRequest.executeAsync())
+            .then(acquiredData => acquiredData.should.be.eql([
+                { sensorId: 1, value: 25 },
+                { sensorId: 2, value: 10 }
+            ]));
+    });
+
     function confirmationFrame() {
         return frameFactory
             .createConfirmationFrame()
